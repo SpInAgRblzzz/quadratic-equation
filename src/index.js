2a6b743666ff0667ed518f16c25d62a40c13b128
SpInAgRblzzz <36811343+SpInAgRblzzz@users.noreply.github.com>
2018-09-16 21:42:19 +0300

    1 attempt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function solveEquation(equation) {
-  // your implementation
+module.exports = function solveEquation(equation) {  
+  var xSquarePosition = equation.indexOf(' * x^2');
+  var a = Number(equation.substring(0 , xSquarePosition))
+  var xPosition = equation.indexOf(' * x ', xSquarePosition);
+  var b = Number(equation[xSquarePosition + 7] + equation.substring(xSquarePosition + 9, xPosition))
+  var c = Number(equation[xPosition + 5] + equation.substring(xPosition + 7))
+  var answer1 = Math.round((-b + Math.sqrt(b * b - 4 * a * c))/(2 * a))
+  var answer2 = Math.round((-b - Math.sqrt(b * b - 4 * a * c))/(2 * a))
+  var solutions  = []
+  solutions.push(answer1, answer2)
+  return solutions.sort(function(a,b){return a-b})
 }
